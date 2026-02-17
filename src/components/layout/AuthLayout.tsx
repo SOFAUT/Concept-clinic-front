e7db8a75e934fe97fd7aaaa39e7ad208229fd6f5
raulhalls <raulcardoso888@gmail.com>
2026-02-17 06:42:34 -0300

    funcionalidade de redirecionamento de retorno para home

--- src/components/layout/AuthLayout.tsx
+++ src/components/layout/AuthLayout.tsx
@@ -1,10 +1,12 @@
 import React, { type ReactNode } from "react";
-import { Box, IconButton, Tooltip } from "@mui/material";
+import { Box, IconButton, Tooltip, Button } from "@mui/material";
 import Brightness4Icon from "@mui/icons-material/Brightness4";
 import Brightness7Icon from "@mui/icons-material/Brightness7";
 import { useState, useMemo } from "react";
 import getTheme from "../../assets/styles/theme";
 import { ThemeProvider, CssBaseline } from "@mui/material";
+import { useNavigate } from "react-router";
+import { APP_ROUTES } from "../../util/constants";
 
 interface AuthLayoutProps {
   children: ReactNode;
@@ -13,6 +15,7 @@ interface AuthLayoutProps {
 const AuthLayout: React.FC<AuthLayoutProps> = ({ children }) => {
   const [themeMode, setThemeMode] = useState<"light" | "dark">("light");
   const theme = useMemo(() => getTheme(themeMode), [themeMode]);
+  const navigate = useNavigate();
   const handleToggleTheme = () =>
     setThemeMode((prev) => (prev === "light" ? "dark" : "light"));
 
@@ -33,6 +36,15 @@ const AuthLayout: React.FC<AuthLayoutProps> = ({ children }) => {
           bgcolor: theme.palette.background.paper,
         }}
       >
+        <Box sx={{ position: "absolute", top: 24, left: 24, zIndex: 10 }}>
+          <Button
+            variant="text"
+            color="inherit"
+            onClick={() => navigate(APP_ROUTES.HOME)}
+          >
+            Voltar para home
+          </Button>
+        </Box>
         <Box sx={{ position: "absolute", top: 24, right: 32, zIndex: 10 }}>
           <Tooltip title={themeMode === "dark" ? "Tema claro" : "Tema escuro"}>
             <IconButton color="primary" onClick={handleToggleTheme}>
